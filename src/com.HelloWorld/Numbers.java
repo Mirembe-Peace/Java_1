8f32ec960791e39a5bb5423f5da3142443026ea9
Mirembe Peace Mercy <peace2de@gmail.com>
2025-08-06 01:17:55 +0300

    Merge branch 'main' of https://github.com/Mirembe-Peace/Java_1 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit. Loops

---
+++ src/com.HelloWorld/Numbers.java
@@ -0,0 +1,66 @@
+package com.HelloWorld;
+
+public class Numbers {
+    public static void main(String[] args){
+        //declaring variables works the same way as in C
+        int a = 54;
+        int b = 8;
+        int c = a + b;
+        System.out.println(c);
+
+        //operations on numbers works the same as in C
+        System.out.println(3 + 4);
+        System.out.println(4 * 8);
+
+        //to declare a variable as a constant in java we use the keyword Final
+        final int d = 30;
+        int x = 45;
+        System.out.println(d * x);
+
+        // logical and && , logical or || , logical not !
+
+        //switch
+        int day = 4;
+        switch(day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+        }
+        //the default keyword is used if there is code to run in case a matching case is not found
+
+        //while loop
+        int countdown = 3;
+        while( countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("Happy new year");
+
+        //do while loop
+        //executes the code block once before checking if the condition is true
+        int i = 7;
+        do {
+            System.out.print(i  + " ");
+            i--;
+        } while(i > 0);
+
+        int numbers = 7;
+        do {
+            System.out.print(numbers);
+            numbers--;
+            System.out.print(numbers);
+        } while(numbers > 0);
+    }
+}
