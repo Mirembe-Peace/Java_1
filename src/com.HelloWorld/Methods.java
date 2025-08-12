b1dd254e9bfe4ffeb137f7c869c6562575bc0758
Mirembe Peace Mercy <peace2de@gmail.com>
2025-08-12 20:58:40 +0300

    Merge branch 'main' of https://github.com/Mirembe-Peace/Java_1 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit. more loops, for each loop

--- src/com.HelloWorld/Methods.java
+++ src/com.HelloWorld/Methods.java
@@ -1,12 +1,60 @@
 package com.HelloWorld;
+// 😕 for Java to accept user input you have to import the scanner class
+
+ import java.util.Scanner;
 
 public class Methods {
     public static void main(String[] args) {
         printer();
+        names("James");
+        names("Michael");
+        names("Peter");
+        name_age("Peter", 23);
+        name_age("Michael", 12);
+        name_age("Carlos", 23);
+        checkAge(23);
+        checkAge(14);
+        System.out.println(calculator(20));
+
+        // first create a scanner object
+        Scanner Obj =  new Scanner(System.in);
+        System.out.println("Enter any positive integers: ");
+        int num_1 = Obj.nextLine();
+
     }
 
     // 🤖When I create my own method i can leave the parentheses empty🤖
     static void printer(){
         System.out.println(" This is my first method");
+    }
+
+    // 🍵 Java methods and arguments
+    static void names(String f_name){
+        System.out.println("Mutegeki " + f_name);
+    }
+    // 💟 multiple parameters
+    static void name_age(String fname, int age){
+        System.out.println("Mukisa " + fname + " is " + age + " years old.");
+    }
+
+    // 😎 method with if else statement
+    static void checkAge(int ages){
+        if (ages < 18 ){
+            System.out.println("You are too young to access this service");
+        }
+        else {
+            System.out.println("You are old enough to access this service");
+        }
+    }
+
+    // 🍵 Java return values
+    // for a method to return a value we use the return keyword
+    static int calculator(int x){
+        return(x + 5);
+    }
+
+    // adding two integers accepting user input
+    static int adder(int a, int b){
+        return(a + b);
     }
 }
